17b5dbecb70523078766bbd594b32337fb8210b2
Abtin Khashayar <akhashayar26@berkeley.edu>
2025-06-30 17:48:01 +0000

    Changed some spacing

--- test_calc.py
+++ test_calc.py
@@ -7,13 +7,13 @@ class TestCalc(unittest.TestCase):
         self.calc = Calc()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(2,3),5)
 
     def test_sub(self):
-        self.assertEqual(self.calc.sub(2, 3), -1)
+        self.assertEqual(self.calc.sub(2,3),-1)
 
     def test_mul(self):
-        self.assertEqual(self.calc.mul(2, 3), 6)
+        self.assertEqual(self.calc.mul(2,3),6)
 
     def test_div(self):
       self.assertEqual(self.calc.div(4,2),2)
@@ -22,4 +22,4 @@ class TestCalc(unittest.TestCase):
       self.assertEqual(self.calc.pow(2,3),8)
 
     def test_sqrt(self):
-      self.assertEqual(self.calc.sqrt(25), 5)
+      self.assertEqual(self.calc.sqrt(25),5)
