f98c1839b92b36ed2f1f08e890f8cb8b5fbdc5c8
Abtin Khashayar <akhashayar26@berkeley.edu>
2025-06-30 17:44:49 +0000

    Changed

--- test_calc.py
+++ test_calc.py
@@ -14,9 +14,12 @@ class TestCalc(unittest.TestCase):
 
     def test_mul(self):
         self.assertEqual(self.calc.mul(2, 3), 6)
+
     def test_div(self):
       self.assertEqual(self.calc.div(4,2),2)
+
     def test_pow(self):
       self.assertEqual(self.calc.pow(2,3),8)
+
     def test_sqrt(self):
       self.assertEqual(self.calc.sqrt(25), 5)
