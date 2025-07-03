6884236c92f7bb6f79499ca704047ea49f9cc054
Abtin Khashayar <akhashayar26@berkeley.edu>
2025-07-03 06:03:07 +0000

    test push yes

--- calc.py
+++ calc.py
@@ -1,6 +1,7 @@
 class Calc:
     """A simple calculator class"""
 
+<<<<<<< HEAD
     def add(self,a,b):
         """Add two numbers together"""
         return a + b
@@ -24,3 +25,29 @@ class Calc:
     def sqrt(self,a):
         """Return the square root of a"""
         return a ** 0.5
+=======
+    def add(self, a, b):
+        """Add two numbers together"""
+        return a + b
+
+    def sub(self, a, b):
+        """Subtract two numbers"""
+        return a - b
+    
+    def mul(self, a, b):
+        """Multiply two numbers"""
+        return a * b
+    
+    def div(self, a, b):
+        """Divide two numbers"""
+        return a / b
+    
+    def pow(self, a, b):
+        """Raise a to the power of b"""
+        return a ** b
+    
+    def sqrt(self, a):
+        """Return the square root of a"""
+        return a ** 0.5
+    
+>>>>>>> 0525ef6 (test push)
